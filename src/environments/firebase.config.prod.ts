e4adee20a5b240efa544ff4be294bc30208c5020
Piotr Narożnik <pnaroznik@gmail.com>
2020-03-22 20:45:11 +0100

    init firebase and prettier

---
+++ src/environments/firebase.config.prod.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig: {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+} = undefined;
