c570577a1a587258497f8eaf23b39dcb701ebae2
Piotr Narożnik <pnaroznik@gmail.com>
2020-08-02 23:22:50 +0200

    feat: add csse dashboard and credit data provider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CsseDashboardComponent } from './csse-dashboard/csse-dashboard.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'covid-19', pathMatch: 'full' },
   { path: 'covid-19', component: DashboardComponent },
+  { path: 'csse', component: CsseDashboardComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
---
+++ src/app/csse-dashboard/csse-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csse-dashboard',
+  templateUrl: './csse-dashboard.component.html',
+})
+export class CsseDashboardComponent {
+  constructor() {
+    console.log('Well hello there! The console errors from www.arcgis.com ain`t mine, sry.');
+  }
+}
